508d6a56f98ef2320bc60545e448a03da1317af3
Jacob Rangel <jacobrangel0628@gmail.com>
2024-09-28 16:16:54 -0500

    Initial Commit

---
+++ RideApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RideApp
+//
+//  Created by Jacob Rangel on 9/28/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RideApp/RideAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RideAppApp.swift
+//  RideApp
+//
+//  Created by Jacob Rangel on 9/28/24.
+//
+
+import SwiftUI
+
+@main
+struct RideAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
