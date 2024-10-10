2885d47d1785bb27f1b89560f103066e8e593529
SageCT <99381480+SageCT@users.noreply.github.com>
2024-10-10 12:03:48 -0500

    Added search bar to RidesView

--- RideApp/RidesView.swift
+++ RideApp/RidesView.swift
@@ -5,15 +5,24 @@
 //  Created by Jacob Rangel on 10/1/24.
 //
 
+import MapKit
 import SwiftUI
 
 struct RidesView: View {
-    @Environment(\.verticalSizeClass) var heightSizeClass: UserInterfaceSizeClass?
-    @Environment(\.horizontalSizeClass) var widthSizeClass: UserInterfaceSizeClass?
+    @Environment(\.verticalSizeClass) var heightSizeClass:
+        UserInterfaceSizeClass?
+    @Environment(\.horizontalSizeClass) var widthSizeClass:
+        UserInterfaceSizeClass?
+
+    @State private var fromAddress: String = ""
+    @State private var destinationAddress: String = ""
+    @State private var driveTime: String =
+        "Next Stop?"
 
     var body: some View {
-        let orientation = DeviceHelper(widthSizeClass: widthSizeClass, heightSizeClass: heightSizeClass)
-        
+        let orientation = DeviceHelper(
+            widthSizeClass: widthSizeClass, heightSizeClass: heightSizeClass)
+
         NavigationView(content: {
             ZStack {
                 Color(hex: "1C1C1E")
@@ -21,25 +30,44 @@ struct RidesView: View {
                 VStack {
                     if orientation.isPortrait(device: .iPhone) {
                         Text("RIDE")
-                            .font(.system(size:24, weight: .black))
+                            .font(.system(size: 24, weight: .black))
                             .foregroundStyle(.white)
                             .padding(.vertical, 4)
                         Spacer()
+
+                        VStack(alignment: .leading) {
+                            TextField(
+                                "", text: $fromAddress,
+                                prompt: Text(
+                                    "\(Image(systemName: "magnifyingglass")) Next Stop?"
+                                )
+                                .foregroundColor(.white)
+                            )
+                            .padding(.vertical, 20)
+                            .background(Color(hex: "303033"))
+                            .cornerRadius(24)
+                            .padding(.horizontal, 16)
+                            .textFieldStyle(.plain)
+                            .foregroundStyle(.white)
+                            .multilineTextAlignment(.center)
+                            .autocorrectionDisabled()
+                            Spacer()
+                        }
+
+                    } else if orientation.isLandscape(device: .iPhone) {
+                    } else {
                     }
-                    else if orientation.isLandscape(device: .iPhone){}
-                    else {}
+
                 }
-                
+
             }
-           
         })
         .navigationViewStyle(StackNavigationViewStyle())
         .tabItem {
             Image(systemName: "mappin.and.ellipse")
             Text("Rides")
         }
-        
-        
+
     }
 }
 
